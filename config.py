b3fc396f3c88fe691ac51a4d79662ef767ba1e52
sid3695 <sid3695@gmail.com>
2016-04-30 20:41:02 +0530

    Oops

---
+++ config.py
@@ -0,0 +1,30 @@
+FPS = 10 # frames per second to update the screen
+WINDOWWIDTH = 640 # width of the program's window, in pixels
+WINDOWHEIGHT = 480 # height in pixels
+SPACESIZE = 50 # width & height of each space on the board, in pixels
+BOARDWIDTH = 8 # how many columns of spaces on the game board
+BOARDHEIGHT = 8 # how many rows of spaces on the game board
+WHITE_TILE = 'WHITE_TILE' # an arbitrary but unique value
+BLACK_TILE = 'BLACK_TILE' # an arbitrary but unique value
+EMPTY_SPACE = 'EMPTY_SPACE' # an arbitrary but unique value
+HINT_TILE = 'HINT_TILE' # an arbitrary but unique value
+ANIMATIONSPEED = 25 # integer from 1 to 100, higher is faster animation
+INFINITY = 99999999
+
+# Amount of space on the left & right side (XMARGIN) or above and below
+# (YMARGIN) the game board, in pixels.
+XMARGIN = int((WINDOWWIDTH - (BOARDWIDTH * SPACESIZE)) / 2)
+YMARGIN = int((WINDOWHEIGHT - (BOARDHEIGHT * SPACESIZE)) / 2)
+
+#              R    G    B
+WHITE      = (255, 255, 255)
+BLACK      = (  0,   0,   0)
+GREEN      = (  0, 155,   0)
+BRIGHTBLUE = (  0,  50, 255)
+BROWN      = (174,  94,   0)
+
+TEXTBGCOLOR1 = BRIGHTBLUE
+TEXTBGCOLOR2 = GREEN
+GRIDLINECOLOR = BLACK
+TEXTCOLOR = WHITE
+HINTCOLOR = BROWN
